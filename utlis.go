00a6c47944cf51709d876d92fc4b7b1c8b0ce29b
Philipp Rauser <philipp.rauser@tuhh.de>
2025-03-28 16:59:02 +0100

    test

--- utlis.go
+++ utlis.go
@@ -1,1 +1,20 @@
-package utils
+package godown
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// input_dir: path to input directory, where markdowns are
+// output_dir: path to output dir where tmpl should be placed
+func New(input_dir string, output_dir string) error {
+	files, err := ioutil.ReadDir(input_dir)
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+	return nil
+}
