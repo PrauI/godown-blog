bb2125461337865edde9507f987580df27e8354f
Philipp Rauser <philipp.rauser@tuhh.de>
2025-03-29 11:12:57 +0100

    test

--- test/main.go
+++ test/main.go
@@ -7,17 +7,11 @@ import (
 )
 
 func main() {
-	articles, err := godown.New("test/input", "test/output")
+	_, err := godown.New("test/input", "test/output")
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for _, article := range articles {
-		fmt.Println(article.name)
-		for _, page := range article.pages {
-			fmt.Println(page.name)
-		}
-	}
 }
---
+++ test/test/input/project1/test.md
@@ -0,0 +1,2 @@
+# hi my name is philipp
+thi is my first blog entry
